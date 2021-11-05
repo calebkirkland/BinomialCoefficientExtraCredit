c5f9cf512cdfe05ee8d00a3ade2812e9ef8a0ece
Caleb Kirkland <calebkirkland2000@gmail.com>
2021-11-05 15:02:54 -0500

    high_resolution_clock fix

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@ using namespace std::chrono;
 void DandC (int n, int k) {
     BinomialCoefficient computation;
 
-    auto start = chrono::steady_clock::now(); // Starting timer
+    auto start = chrono::high_resolution_clock::now(); // Starting timer
     ios_base::sync_with_stdio(false); // Unsync IO of C and C++
 
     int result = computation.DandCAlgorithm(n, k);
 
-    auto end = chrono::steady_clock::now(); // Ending the timer
+    auto end = chrono::high_resolution_clock::now(); // Ending the timer
     double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
     time_taken *= 1e-9; // Nano * 10^-9 = seconds
 
@@ -24,12 +24,12 @@ void DandC (int n, int k) {
 void Memo (int n, int k) {
     BinomialCoefficient computation;
 
-    auto start = chrono::steady_clock::now(); // Starting timer
+    auto start = chrono::high_resolution_clock::now(); // Starting timer
     ios_base::sync_with_stdio(false); // Unsync IO of C and C++
 
     int result = computation.MemoAlgorithm(n, k);
 
-    auto end = chrono::steady_clock::now(); // Ending the timer
+    auto end = chrono::high_resolution_clock::now(); // Ending the timer
     double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
     time_taken *= 1e-9; // Nano * 10^-9 = seconds
 
@@ -39,12 +39,12 @@ void Memo (int n, int k) {
 void DynamicProgramming(int n, int k) {
     BinomialCoefficient computation;
 
-    auto start = chrono::steady_clock::now(); // Starting timer
+    auto start = chrono::high_resolution_clock::now(); // Starting timer
     ios_base::sync_with_stdio(false); // Unsync IO of C and C++
 
     unsigned int result = computation.DynamicProgrammingAlgorithm(n, k);
 
-    auto end = chrono::steady_clock::now(); // Ending the timer
+    auto end = chrono::high_resolution_clock::now(); // Ending the timer
     double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
     time_taken *= 1e-9; // Nano * 10^-9 = seconds
 
